4efb3914245b87c9f9739e77b08b35814595e0c7
maxpdr96 <maxpdr96@gmail.com>
2025-04-01 10:23:18 -0300

    Primeiro commit

---
+++ src/main/java/com/hidarisoft/poslogisticamicroservice/PosLogisticaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.hidarisoft.poslogisticamicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosLogisticaMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosLogisticaMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hidarisoft/poslogisticamicroservice/PosLogisticaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hidarisoft.poslogisticamicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosLogisticaMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
